f2ccc61ca1b554a63d5a374738df05720980af6f
Cody Chance <cachance@iastate.edu>
2017-11-06 18:34:55 -0600

    Added HTML Templates

--- Flask Project/myflaskapp.py
+++ Flask Project/myflaskapp.py
@@ -1,6 +1,4 @@
 from flask import Flask, render_template, request
-import markdown as md
-import csv
 
 app = Flask(__name__)
 
@@ -9,46 +7,49 @@ WEB_APP_NAME = "BFC PORTAL"
 
 @app.route('/')
 @app.route('/home')
-@app.route('home/<name>')
 def home(name=WEB_APP_NAME):
     return render_template("home.html", content=name)
 
 
-@app.route('/employeePortal/')
-@app.route('/employeePortal/<name>')
-def EmployeePortal(name=WEB_APP_NAME):
-    return render_template("employeePortal.html", content=name)
+@app.route('/login', methods=['POST'])
+def employeeLogin():
+    user = request.form['user']
+    password = request.form['password']
+
+    if user == 'cody' and password == '12345':
+        return render_template("login_good.html", user=user)
+    return render_template("login_form.html")
+
+
+@app.route('/login', methods=['GET'])
+def employeeLogin_Page():
+    return render_template("login_form.html")
 
 
 @app.route('/membership/')
-@app.route('/membership/<name>')
 def membership(name=WEB_APP_NAME):
     return render_template("membership.html", content=name)
 
 
-@app.route('/courses/')
-@app.route('/courses/<name>')
-def courses(name=WEB_APP_NAME):
-    return render_template("courses.html", content=name)
+@app.route('/course/')
+def course(name=WEB_APP_NAME):
+    return render_template("course.html", content=name)
 
 
 @app.route('/calendar/')
-@app.route('/calendar/<name>')
 def calendar(name=WEB_APP_NAME):
     return render_template("calendar.html", content=name)
 
 
 @app.route('/employee/')
-@app.route('/employee/<name>')
 def employee(name=WEB_APP_NAME):
     return render_template("employee.html", content=name)
 
 
-@app.route('/products/')
-@app.route('/products/<name>')
-def products(name=WEB_APP_NAME):
-    return render_template("products.html", content=name)
+@app.route('/product/')
+def product(name=WEB_APP_NAME):
+    return render_template("product.html", content=name)
 
 
-if __name__=='__main__':
+if __name__ == '__main__':
     app.run(host='0.0.0.0', port=8080, debug=True)
